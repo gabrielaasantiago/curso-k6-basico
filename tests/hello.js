81524c639b65acfbee8bddd2e3fc67640ff77c14
gabrielaasantiago <gabrielaasantiago@outlook.com>
2024-01-02 11:43:33 -0300

    inserindo check de status

--- tests/hello.js
+++ tests/hello.js
@@ -1,11 +1,16 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
 //export const options = {
 //    vus: 10,
 //    duration: '30s',
 //};
 
 export default function () {
-    http.get('http://localhost:3333');
+    const res = http.get('http://localhost:3333');
+    
+    check(res, {
+        'status should be 200': (r) => r.status === 200
+    });
+
     sleep(1);
 }
