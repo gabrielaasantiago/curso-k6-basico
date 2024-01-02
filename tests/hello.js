dd9a42dc96783c8d36a2ab63fd19837b837b98ed
gabrielaasantiago <gabrielaasantiago@outlook.com>
2024-01-02 16:03:12 -0300

    gerando reports em html

--- tests/hello.js
+++ tests/hello.js
@@ -1,9 +1,13 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
-//export const options = {
-//    vus: 10,
-//    duration: '30s',
-//};
+
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export function handleSummary(data) {
+    return {
+        "summary.html": htmlReport(data),
+    };
+}
 
 export default function () {
     const res = http.get('http://localhost:3333');
